1241d625f2458df494892cbc9cfed95a67a220eb
Ana Ruiz <ruizana393@gmail.com>
2021-10-14 21:19:48 -0400

    User_in_progress

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password_digest, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email.strip!
+    email.downcase!
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+end
